8a49235c237f35dc701fd107661df3bd291e9664
MuzamilEng <muzekhan777@gmail.com>
2025-10-30 02:59:02 +0500

    updated condition

--- src/app/staff-dashboard/page.tsx
+++ src/app/staff-dashboard/page.tsx
@@ -730,23 +730,23 @@ export default function Dashboard() {
             }`}
         >
           <div className="p-6">
-            {session?.user?.role === "Staff" && (
-              <button
-                ref={createSnapLinkButtonRef}
-                className="snaplink-btn"
-                onClick={() => snapInputRef.current?.click()}
-                disabled={uploading}
-              >
-                {uploading ? (
-                  <span className="flex items-center">
-                    <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white mr-2"></div>
-                    Uploading...
-                  </span>
-                ) : (
-                  "📁Create DocLatch"
-                )}
-              </button>
-            )}
+            {/* {session?.user?.role === "Staff" && ( */}
+            <button
+              ref={createSnapLinkButtonRef}
+              className="snaplink-btn"
+              onClick={() => snapInputRef.current?.click()}
+              disabled={uploading}
+            >
+              {uploading ? (
+                <span className="flex items-center">
+                  <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white mr-2"></div>
+                  Uploading...
+                </span>
+              ) : (
+                "📁Create DocLatch"
+              )}
+            </button>
+            {/* )} */}
             <input
               type="file"
               ref={snapInputRef}
